eb4b77d1b41e56ab943295e70055988ab4029f72
Grzegorz Kossowski <grko@demant-technology.com>
2019-06-26 11:56:11 +0200

    Initial project

---
+++ CodedViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UIKit;
+
+namespace AutoLayoutConstraints
+{
+    public class CodedViewController : UIViewController
+    {
+        private UILabel label;
+
+        public CodedViewController()
+        {
+            TabBarItem.Title = "Coded";
+            TabBarItem.Image = UIImage.FromBundle("Square");
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            label = new UILabel
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                Text = "Coded UI",
+                Font = UIFont.PreferredTitle2,
+            };
+
+            View.AddSubviews(label);
+
+            View.BackgroundColor = UIColor.White;
+
+            var guide = View.SafeAreaLayoutGuide;
+
+            NSLayoutConstraint.ActivateConstraints(new[]
+            {
+                label.CenterXAnchor.ConstraintEqualTo(guide.CenterXAnchor),
+                label.CenterYAnchor.ConstraintEqualTo(guide.CenterYAnchor),
+            });
+        }
+    }
+}
---
+++ XibViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+
+namespace AutoLayoutConstraints
+{
+    public partial class XibViewController : UIViewController
+    {
+        public XibViewController() : base("XibViewController", null)
+        {
+            TabBarItem.Title = "XIB";
+            TabBarItem.Image = UIImage.FromBundle("Circle");
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
